4bc8b67d5604e81521f491723c19424cc0465b1b
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-01-26 18:49:22 +0900

    Added Projectile Destroy logic

--- MissionDemolition/Assets/Scripts/Projectile.cs
+++ MissionDemolition/Assets/Scripts/Projectile.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
+    Transform _transform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_transform.position.x < -20 || _transform.position.y < -20 || _transform.position.x > 150)
+        {
+            _ = StartCoroutine(nameof(OnDestroyed));
+        }
     }
+
+    IEnumerator OnDestroyed()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+
+    
 }
