25060a8c9270fd5b83347a4258a4eb2ad8c8f9e5
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-01-31 22:05:32 +0900

    Added basic card support with interatablity

---
+++ CardGame/Assets/Scripts/Card.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(BoxCollider2D))]
+public class Card : MonoBehaviour
+{
+    // Card Component.
+    // Made of enum Sign and number.
+    // Rendered using SpriteRenderer.
+    public enum Sign { Spade, Diamond, Heart, Clover}
+    public enum Number { NotANumber, Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King}
+    public Sign sign;
+    public Number number;
+
+    public TextMesh signText;
+    public TextMesh numberText;
+
+    bool isHidden = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateCardShape();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Functions about Initial Setting
+    public void SetShape(Sign sign, Number number)
+    {
+        this.sign = sign;
+        this.number = number;
+        UpdateCardShape();
+    }
+
+    // Functions about Updatating visual according to value
+    void UpdateCardShape()
+    {
+        signText.text = Enum.GetName(typeof(Sign), sign);
+        numberText.text = Enum.GetName(typeof(Number), number);
+    }
+    void UpdateCardVisual()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder = isHidden ? 1 : 0;
+    }
+
+    //Utility function that acts something to card
+
+    public void Hide()
+    {
+        isHidden = true;
+        UpdateCardVisual();
+    }
+
+    public void Show()
+    {
+        isHidden = false;
+        UpdateCardVisual();
+    }
+
+
+
+
+}
---
+++ CardGame/Assets/Scripts/Interactable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Interactable : MonoBehaviour
+{
+    // 인터렉션을 허용할 오브젝트에 하나씩 붙이는 방식으로 작동.
+    // 장점: 유니티의 네이티브 함수를 이용한다.
+    // 단점: 필요한 곳에 일일히 붙여야 한다.
+
+    Vector3 delta = Vector3.zero;
+
+    private void OnMouseOver()
+    {
+        //호버를 할 때
+        //뭔가 테두리를 예쁘게 한다거나? SendMessage가 적절할 듯.
+    }
+    private void OnMouseDown()
+    {
+        // 클릭을 했을 때
+        // 현재 마우스 위치와 transform 위치 사이의 차이를 delta에 기록한다.
+        Vector3 objectPosition = transform.position;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = Camera.main.nearClipPlane;
+        delta = objectPosition - mousePosition;
+
+    }
+
+    private void OnMouseDrag()
+    {
+        // 드래그 중에
+        // delta를 유지하면서 mouse position에 따라간다.
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = Camera.main.nearClipPlane;
+        transform.position = mousePosition + delta;
+    }
+
+    private void OnMouseUp()
+    {
+        // 클릭이 끝나면
+        // delta를 초기화한다.
+        delta = Vector3.zero;
+    }
+
+}
