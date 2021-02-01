5e9732e210063b32e46b382b0e2a1d57bcfbf95a
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-02-01 20:28:48 +0900

    Added MenuButton script for menu interaction using builtin OnClick()

---
+++ CardGame/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class MenuButton : MonoBehaviour
+{
+    Button button;
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+    public void LoadScene()
+    {
+        button.interactable = false;
+        StartCoroutine(SceneControl.current.LoadScene("Map"));
+        button.interactable = true;
+    }
+
+    public void UnloadScene()
+    {
+        button.interactable = false;
+        StartCoroutine(SceneControl.current.UnloadAllScene());
+        button.interactable = true;
+    }
+
+}
