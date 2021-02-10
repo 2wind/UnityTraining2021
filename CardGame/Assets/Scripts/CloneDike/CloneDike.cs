dc3ac18cb9065427f5c0ed376fc5962ced9e4926
Kim Yongwook <kim.yongwook.snu@gmail.com>
2021-02-10 17:16:06 +0900

    Moved scripts to according folder

---
+++ CardGame/Assets/Scripts/CloneDike/CloneDike.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneDike : MonoBehaviour
+{
+    public GameObject deck;
+    public List<GameObject> slots;
+
+    public List<GameObject> results;
+
+
+    void Awake()
+    {
+        deck = GameObject.Find("Deck");
+        slots = new List<GameObject>();
+        slots.AddRange(GameObject.FindGameObjectsWithTag("Slot"));
+
+        results = new List<GameObject>();
+        // Add result slots(4) later
+    }
+
+    private void Start()
+    {
+        // from deck, pop 7 to 1 cards and push them to slots accordingly
+
+
+
+        // track score
+        // use observer pattern?
+        // For each event happens, update score on scoreboard.
+        // Singleton to subscribe?
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
